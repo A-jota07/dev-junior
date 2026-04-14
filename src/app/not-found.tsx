d893ee56315db732b7941e3a021c903188ab4ad4
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2026-04-14 16:34:21 -0400

    Pagina não encontrada

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import ErrorMessage from "@/Components/ErrorMessage";
+
+export default function NotFoundPage() {
+   return (
+      <ErrorMessage
+         pageTitle='Página não encontrada'
+         contentTitle='404'
+         content='Erro 404 - A página que você está tentando acessar não existe
+                  neste site.'
+      />
+   );
+}
