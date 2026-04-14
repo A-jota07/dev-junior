0e0143f1c8043c5e6c6ba7bd0cba42204955f4fb
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2026-04-14 16:33:01 -0400

    Mensagem de error do servidor

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import ErrorMessage from '@/Components/ErrorMessage';
+import { useEffect } from 'react';
+
+type RootErrorPageProps = {
+   error: Error;
+   reset: () => void;
+};
+
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+   useEffect(() => {}, [error]);
+
+   return (
+      <ErrorMessage
+         pageTitle='Internal Server Error'
+         contentTitle='501'
+         content='Ocorreu um erro do qual nossa aplicação não conseguiu ser recuperar. Tente novamente mais tarde.'
+      />
+   );
+}
