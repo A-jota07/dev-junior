2343124f827f4e62419133afdee64aa29a4fa1f4
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2026-04-14 16:13:46 -0400

    Adicionando lucide icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
     >
       <body>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default async function HomePage() {
   return (
     <>
     Home do site
+
+    <footer>
+      <p className='text-6xl font-bold text-center py-8'>Footer</p>
+    </footer>
     </>
   )
 }
